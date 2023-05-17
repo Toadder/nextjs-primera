e6c4249c95255a9b1a80551ecdd781fbe8168538
toadder <zheka20042006@gmail.com>
2023-05-17 13:10:21 +0300

    Fix bugs

--- src/components/common/Map/index.jsx
+++ src/components/common/Map/index.jsx
@@ -16,7 +16,7 @@ const Map = () => {
 					}}
 					allowFullScreen=''
 					loading='lazy'
-					referrerpolicy='no-referrer-when-downgrade'
+					referrerPolicy='no-referrer-when-downgrade'
 				></iframe>
 			)}
 		</div>
